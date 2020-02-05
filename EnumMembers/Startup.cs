b37d7bd7c795d5d0864376f8d0877ae71ecb7b16
Sebastian <ja@sebastianbusek.cz>
2020-02-05 10:59:06 +0100

    feat(): removed global JsonStringEnumConverter

--- EnumMembers/Startup.cs
+++ EnumMembers/Startup.cs
@@ -20,10 +20,12 @@ namespace EnumMembers
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddJsonOptions(options =>
-            {
-                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-            }); ;
+            services.AddControllers();
+
+            //services.AddControllers().AddJsonOptions(options =>
+            //{
+            //    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
